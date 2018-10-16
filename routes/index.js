8b0b22324667a8be1919ed5de8d32941f7d59db0
irvin <irvinlucho@gmail.com>
2018-10-16 12:16:43 -0500

    feat(re(admin) agregando vista login

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/admin', function(req, res, next) {
+    res.render('login', { title: 'Login' ,data:[
+            {name:"irvin"},{name:"luis"}
+        ]});
+});
 
 module.exports = router;
